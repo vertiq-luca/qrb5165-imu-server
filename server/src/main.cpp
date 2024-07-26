152195ae0082fb30160df5fc5f50c9a26c68d67b
Luca Scheuer <luca.scheuer@iq-control.com>
2024-07-26 08:50:42 -0500

    initial working dynamic notch filter

--- server/src/main.cpp
+++ server/src/main.cpp
@@ -64,6 +64,8 @@
 #define MAX_FIFO_SAMPLES 300
 
 #define NUM_IMU_AXIS 6
+#define NUM_IMU_HARMONICS 3
+#define ONE_BY_SIXTY 0.016666667
 
 // extern debug vars shared across anything that might want to print debug info
 #include "global_debug_flags.h"
@@ -87,6 +89,7 @@ static cJSON* pipe_info_json[N_IMUS];
 
 static int32_t motor_rpm[8];
 static int num_motors = 2;
+static const double min_rad_s = 150;
 pthread_mutex_t rpm_mtx;
 
 rc_filter_t motor_notches[N_IMUS][8][NUM_IMU_AXIS];
@@ -339,8 +342,8 @@ static void* _read_thread_func(void* context)
 	static int64_t next_time = 0;
 	int id = (intptr_t)context;
 	int ret, i, jj, kk;
-	double c, num[2], den[2], dt;
 	int32_t local_motor_rpm[8];
+	double local_motor_w[8];
 	imu_data_t data[MAX_FIFO_SAMPLES];
 	imu_data_t filtered_data[MAX_FIFO_SAMPLES];
 	int packets_read;		// number of packets read from fifo each cycle
@@ -390,23 +393,23 @@ static void* _read_thread_func(void* context)
 			local_motor_rpm[i] = motor_rpm[i];
 		}
 		pthread_mutex_unlock(&rpm_mtx);
-				
+		
 		for(i = 0; i < packets_read; i++)
 		{
 			for (jj = 0; jj < num_motors; jj++)
 			{
 				//rc_filter_t motor_notches[N_IMUS][8][NUM_IMU_AXIS];
-
+				local_motor_w[jj] = local_motor_rpm[jj] * 2 * M_PI * ONE_BY_SIXTY;
+				
+				if (local_motor_w[jj] < min_rad_s)
+				{
+					local_motor_w[jj] = min_rad_s;
+				}
+				
 				// update filters to motor RPM based on most recent motor RPM
-				dt = 1 / imu_sample_rate_hz[id];
-				c = dt/local_motor_rpm[jj]; // these can be updated realtime to match my rpm
-				num[0] = c;
-				num[1] = 0.0;
-				den[0] = 1.0;
-				den[1] = c - 1.0;
 				for (kk = 0; kk < NUM_IMU_AXIS; kk++)
 				{
-					rc_filter_alloc_from_arrays(&motor_notches[id][num_motors][kk],dt,num,2,den,2);
+					update_stop_wc(&motor_notches[id][jj][kk], local_motor_w[jj], 250, 100);
 				}
 				filtered_data[i].accl_ms2[0] = rc_filter_march(&motor_notches[id][jj][0], data[i].accl_ms2[0]);
 				filtered_data[i].accl_ms2[1] = rc_filter_march(&motor_notches[id][jj][1], data[i].accl_ms2[1]);
@@ -415,6 +418,10 @@ static void* _read_thread_func(void* context)
 				filtered_data[i].gyro_rad[1] = rc_filter_march(&motor_notches[id][jj][4], data[i].gyro_rad[1]);
 				filtered_data[i].gyro_rad[2] = rc_filter_march(&motor_notches[id][jj][5], data[i].gyro_rad[2]);
 			}
+		}
+		
+		if(packets_read>0){
+			pipe_server_write(N_IMUS * 2 + id, (char*)data, packets_read*sizeof(imu_data_t));
 		}
 
 		for(i = 0; i < packets_read; i++)
@@ -440,9 +447,6 @@ static void* _read_thread_func(void* context)
 		if(en_print_timestamps){
 			for(i=0;i<packets_read;i++) printf("%10" PRId64 "\n", data[i].timestamp_ns);
 		}
-		pthread_mutex_lock(&rpm_mtx);
-		data[0].temp_c = float(motor_rpm[0]);
-		pthread_mutex_unlock(&rpm_mtx);
 		// send to pipe
 		if(packets_read>0){
 			//int64_t t1 = my_time_monotonic_ns();
@@ -734,6 +738,28 @@ int main(int argc, char* argv[])
 			fprintf(stderr, "WARNING: failed to create FFT pipe for IMU %d\n", i);
 			continue;
 		}
+		// pre filtered IMU data
+		pipe_info_t info3 = { \
+			"",\
+			"",\
+			"imu_data_t",\
+			PROCESS_NAME,\
+			IMU_RECOMMENDED_PIPE_SIZE,\
+			0};
+		
+		strcpy(info3.name, names[i]);
+		strcat(info3.name, "_pre");
+		printf("%s\r\n", info3.name);
+
+		pipe_server_set_control_cb(N_IMUS * 2 + i, _control_pipe_handler, NULL);
+		pipe_server_set_connect_cb(N_IMUS * 2 + i, _connect_handler, NULL);
+		pipe_server_set_disconnect_cb(N_IMUS * 2 + i, _disconnect_handler, NULL);
+
+		if(pipe_server_create(N_IMUS * 2 + i, info3, 0)){
+			imu_enable[i]=0; // flag imu as not enabled since it failed
+			imu_close(i);
+			continue;
+		}
 
 		// TODO fetch the real ODR from the timestamp filter while running
 		// for now this works for VOXL2
@@ -771,8 +797,7 @@ int main(int argc, char* argv[])
 			for (int kk = 0; kk < NUM_IMU_AXIS; kk++)
 			{
 				motor_notches[i][jj][kk] = RC_FILTER_INITIALIZER;
-				rc_filter_butterworth_lowpass(&motor_notches[i][jj][kk], 2, imu_sample_rate_hz[i], 100);
-				// rc_filter_first_order_lowpass(&motor_notches[i][jj][kk], 0.001, 50);
+				rc_filter_bandstop(&motor_notches[i][jj][kk], 2,  1.0 / imu_sample_rate_hz[i], 315, 250, 40);
 			}
 		}
 	}
